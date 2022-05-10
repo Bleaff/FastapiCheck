d57c327bce3330940bded6cda8cb12cca82f8904
Kharlen <59150974+Serjo750@users.noreply.github.com>
2022-05-11 00:33:43 +0300

    Word is done

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, MetaData, Integer, Computed
 import psycopg2
 
 db_name = 'quest_db'
@@ -18,8 +19,8 @@ db = create_engine(db_string)
 
 def add_new_row(id, question, answer, created_at):
     # Insert a new number into the 'numbers' table.
-    db.execute("INSERT INTO quest (id, question, answer, creadet_at) "+\
-        "VALUES (" + f'{id}, {question}, {answer}, {created_at});')
+    insert_string = "INSERT INTO quest (id, question, answer, created_at) VALUES " + f"({id}, %s, %s, %s);"
+    db.execute(insert_string, (question, answer, created_at))
 
 def check_in_table(id):
     result = db.execute(
@@ -50,18 +51,42 @@ class Question(BaseModel):
 
 
 
+
+def get_request(q_num):
+    res = requests.get(f'https://jservice.io/api/random?count={q_num}')
+    raw_json = res.json()
+    parsed = list()
+    for it in raw_json:
+        parsed.append(Question.parse_obj(it))
+    return parsed
+
+def get_unique_request():
+    parsed = get_request(1)
+    while check_in_table(parsed[0].id):
+        parsed = get_request(1)
+    return parsed[0].id, parsed[0].question, parsed[0].answer, parsed[0].created_at
+
+
 app = FastAPI()
 #Just parse data to table
 @app.get("/")
 async def get_model(question_num : int):
     res = requests.get(f'https://jservice.io/api/random?count={question_num}')
     raw_json = res.json()
-    parsed = list()
     for it in raw_json:
-        parsed.append(Question.parse_obj(it))
-    check_in_table(parsed[0].id)
+        last = Question.parse_obj(it)
+        if check_in_table(last.id):
+            last = get_unique_request()
+            add_new_row(last[0], last[1], last[2], last[3])
+        else:
+            add_new_row(last.id , last.question, last.answer, last.created_at)    
+    return last
 
-    print(type(result))
-    return True
-
-    
+@app.get("/selected")
+async def get_table_info():
+    with db.connect() as conn:
+        result = conn.execution_options(stream_results=True).execute(str("select * from quest;"))
+        res_list = list()
+        for it in result:
+            res_list.append(it)
+    return res_list
--- db/quest.sql
+++ db/quest.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS quest(
     id integer NOT NULL,
     question text NOT NULL,
     answer text NOT NULL,
-    creadet_at text NOT NULL
+    created_at text NOT NULL
 );
 
